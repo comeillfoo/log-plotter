5e98e5307ebe63556e2ef7f34ad52181d63292b8
come_ill_foo <rayveleaveme34@inbox.ru>
2023-03-11 00:29:36 +0000

    feat: working module + parsing values into floats if possible

--- __main__.py
+++ __main__.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
-import matplotlib.pyplot as plt
+import os
+from sys import argv
+from logplotterapi import collect_logs, Axis
+from matplotlib import pyplot as plt
 
 
-def main():
-    
+def main(argv) -> int:
+    if len(argv) < 3:
+        return -1
+
+    _, logs, parameter = argv
+    functions = collect_logs(logs, parameter)
+    fig, ax = plt.subplots(figsize=(7.20, 4.80))
+    for (label, data) in functions.items():
+        ax.plot(data[Axis.X], data[Axis.Y], marker='o', label=label)
+    ax.set_title(parameter)
+    plt.legend(bbox_to_anchor=(1.0, 1.0), loc='upper left')
+    fig.savefig(f'{os.path.basename(logs)}.{parameter}.png', bbox_inches='tight')
+    return 0
 
 
 if __name__ == '__main__':
-    main()
+    exit(main(argv))
--- logplotterapi.py
+++ logplotterapi.py
@@ -8,36 +8,58 @@ Axis = Enum('Axis', ['X', 'Y'])
 
 
 def parse_record(record: str, ts_format: str, sep: str) -> Tuple[float, str, str, str]:
-    timestamp, log_level, class_function, text = record.split(sep, 3)
+    values = record.split(sep, 3)
+    if len(values) != 4:
+        raise ValueError(f'Line "{record}" doesn\'t contain enough parameters')
+    timestamp, log_level, class_function, text = values
     timestamp = datetime.strptime(timestamp, ts_format)
     return timestamp.timestamp(), log_level, class_function, text
 
 
-def parse_parameter(text: str, pattern: re.Pattern, groups: list):
-    match = pattern.match(text)
-    return bool(match), tuple(map(lambda group: match.group(group), groups))
+def parse_parameter(message: str, pattern: re.Pattern, groups: list):
+    match = pattern.match(message)
+    return (False, (None, None)) if match is None else (True, tuple(map(lambda group: match.group(group), groups)))
+
+
+def add_vertex(vertices: dict, subgroup: str, x: float, y):
+    vertices[subgroup] = vertices.get(subgroup, { Axis.X: [], Axis.Y: [] })
+    vertices[subgroup][Axis.X].append(x)
+    vertices[subgroup][Axis.Y].append(y)
+
+
+def parse_float(raw_number: str) -> Tuple[bool, any]:
+    try:
+        number = float(raw_number)
+        return True, number
+    except (TypeError, ValueError) as e:
+        return False, e
 
 
 def collect_logs(logs_path: str, parameter: str, ts_format: str = '%Y-%m-%d %H:%M:%S.%f', sep: str = '|'):
-    pattern = re.compile(f'(?P<prefix>.+)?{parameter}:\s*(?P<value>.+)')
-    group_names = ['prefix', 'value']
-    result = dict()
+    pattern = re.compile(f'(?P<prefix>.+)?{re.escape(parameter)}:\s*(?P<value>.+)')
+    pattern_group_names = ['prefix', 'value']
+    functions_table = dict()
     with open(logs_path) as logs:
         content = logs.readlines()
-        start_msecs, _, _, text = parse_record(content[0], ts_format, sep)
-        is_match, (prefix, value) = parse_parameter(text, pattern, group_names)
+        # parse first line to get the start time
+        start_msecs, _, _, message = parse_record(content[0], ts_format, sep)
+        is_match, (prefix, value) = parse_parameter(message, pattern, pattern_group_names)
         if is_match:
-            result[prefix][Axis.X].append(0.0)
-            result[prefix][Axis.Y].append(value)
+            is_parsed, number = parse_float(value)
+            add_vertex(functions_table, (prefix or '').strip(" \t\n."), 0.0, number if is_parsed else value)
 
-        for record in content[:1]:
-            msecs, _, _, text = parse_record(record, ts_format, sep)
-            is_match, (prefix, value) = parse_parameter(text, pattern, group_names)
-            if is_match:
-                result[prefix][Axis.X].append(msecs - start_msecs)
-                result[prefix][Axis.Y].append(value)
+        # parse the rest lines
+        for record in content[1:]:
+            try:
+                msecs, _, _, message = parse_record(record, ts_format, sep)
+                is_match, (prefix, value) = parse_parameter(message, pattern, pattern_group_names)
+                if is_match:
+                    is_parsed, number = parse_float(value)
+                    add_vertex(functions_table, (prefix or '').strip(" \t\n."), (msecs - start_msecs) * 1000, number if is_parsed else value)
+            except ValueError as e:
+                print(e.__str__().strip())
 
-    return result
+    return functions_table
 
 
 
